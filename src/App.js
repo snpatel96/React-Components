ba07817b1569d97c0110a00f5316df39c0ccd5f0
snehal patel <snpatel.dev@gmail.com>
2022-11-07 17:23:34 -0500

    #2 created a component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
 import './App.css';
-import { faker } from '@faker-js/faker';
+import CommentDetail from './CommentDetail';
 
 function App() {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            John Doe
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice Blog Post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            John Doe
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice Blog Post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            John Doe
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice Blog Post!</div>
-        </div>
-      </div>
+      <CommentDetail />
     </div>
   );
 }
